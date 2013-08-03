71114d0e6a419521cf07a798ed35b768a7c33382
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-03 00:05:41 -0500

    Symmetric Commit Solution

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -2,7 +2,7 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to 
 
-class CreateTeachers < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
     create_table :students do |t|
